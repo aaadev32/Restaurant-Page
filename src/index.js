6aa57fad71edb944ac7a557f7071bfb286fcb4c3
rusterz <aaadev32@gmail.com>
2022-06-12 21:20:40 -0700

    added image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('test passed.')
+import scene from './restaurant.jpg';
+
+function component(){
+    const element = document.createElement('div');
+    const newImg = new Image();
+    newImg.src = scene;
+    element.appendChild(newImg);
+
+    return element;
+}
+
+document.getElementById('content').appendChild(component());
