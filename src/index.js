eba6d84b110e53847b730840e2cb5c1bbe165f78
rusterz <aaadev32@gmail.com>
2022-06-13 19:20:27 -0700

    added background and header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import scene from './restaurant.jpg';
+import './style.css'
 
-function component(){
+function header() {
     const element = document.createElement('div');
-    const newImg = new Image();
-    newImg.src = scene;
-    element.appendChild(newImg);
-
-    return element;
+    const h2 = document.createElement('h2');
+    h2.textContent = 'Callisto\'s Fine Dine';
+    document.getElementById('header').appendChild(h2);
 }
 
-document.getElementById('content').appendChild(component());
+function background() {
+    const element = document.createElement('div');
+
+    //background
+    const newImg = new Image();
+    newImg.src = scene;
+    element.id = 'background';
+
+    document.body.style.backgroundImage = `url(${scene})`;
+
+}
+
+function content() {
+
+}
+
+function footer() {
+
+}
+
+document.body.appendChild(content(), header(), background(), footer());
