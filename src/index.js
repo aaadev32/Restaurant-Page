c7c446d4eff11d652d082de7a4005b2395d8d7b4
rusterz <aaadev32@gmail.com>
2022-06-14 18:58:38 -0700

    added nav bar tabs

--- src/index.js
+++ src/index.js
@@ -2,30 +2,74 @@ import scene from './restaurant.jpg';
 import './style.css'
 
 function header() {
-    const element = document.createElement('div');
+    //h2
     const h2 = document.createElement('h2');
-    h2.textContent = 'Callisto\'s Fine Dine';
+    h2.textContent = 'Callisto\'s Fine Dinery';
     document.getElementById('header').appendChild(h2);
+
+    //#nav menu tabs
+    const navContainer = document.createElement('div');
+    const navTabs = document.createElement('div');
+    navContainer.id = 'nav-container';
+    document.getElementById('header').appendChild(navContainer);
+
+    const homeTab = document.createElement('div');
+    const menuTab = document.createElement('div');
+    const contactTab = document.createElement('div');
+
+    homeTab.textContent = 'Home';
+    homeTab.className = 'nav-tabs';
+    homeTab.onclick = onClick();
+
+    menuTab.textContent = 'Menu';
+    menuTab.className = 'nav-tabs';
+    menuTab.onclick = onClick();
+
+    contactTab.textContent = 'Contact';
+    contactTab.className = 'nav-tabs';
+    contactTab.onclick = onClick();
+
+    document.getElementById('nav-container').append(homeTab, menuTab, contactTab);
 }
 
 function background() {
-    const element = document.createElement('div');
+    const div = document.createElement('div');
 
     //background
     const newImg = new Image();
     newImg.src = scene;
-    element.id = 'background';
+    div.id = 'background';
 
     document.body.style.backgroundImage = `url(${scene})`;
 
 }
 
 function content() {
+    const div = document.createElement('div');
+
+    //#create home content for home tab
+
+    //#create grid for food menu
+
+    //#create contact content for content tab
 
 }
 
 function footer() {
-
+    //#create footer bar with trademarks, associations and other info.
 }
 
+function onClick(element) {
+    if (element.textContent == 'Home') {
+
+    } else if (element.textContent == 'Menu') {
+
+    } else if (element.textContent == 'Contact')
+}
+
+const generateTab = (() => {
+    //const homeTab generate page here
+    //const menuTab generate page here
+    //const contactTab generate page here
+})();
 document.body.appendChild(content(), header(), background(), footer());
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    devtool: 'inline-source-map',
+
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
@@ -10,30 +12,30 @@ module.exports = {
     module: {
 
         rules: [
-    
-          {
-    
-            test: /\.css$/i,
-    
-            use: ['style-loader', 'css-loader'],
-    
-          },
-          {
-    
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-    
-            type: 'asset/resource',
-    
-          },
-          {
-    
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-    
-            type: 'asset/resource',
-    
-          },
-          
+
+            {
+
+                test: /\.css$/i,
+
+                use: ['style-loader', 'css-loader'],
+
+            },
+            {
+
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+                type: 'asset/resource',
+
+            },
+            {
+
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+                type: 'asset/resource',
+
+            },
+
         ],
-    
-      },
-    };
+
+    },
+};
